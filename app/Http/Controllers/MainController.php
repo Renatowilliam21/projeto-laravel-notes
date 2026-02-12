ca4f5d5aa5792d88e6d2ebefbb7506717d95c611
Renato William Rodrigues de Souza <renatowilliam21@gmail.com>
2026-02-11 22:15:16 -0300

    Otimizacao do codigo

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 
@@ -30,17 +31,40 @@ public function newNote(){
 
 public function editNote($id){
 
-$id = Crypt::decrypt($id);
 
-echo $id;
+$id = $this->decryptId($id);
+echo "ESTOU EDITANDO A NOTA CUJO O ID É $id";
+
+
+
 
 }
 
 public function deleteNote($id){
 
 
+$id = $this->decryptId($id);
+echo "ESTOU DELETANDO A NOTA CUJO O ID É $id";
+
+
 }
 
+private function decryptId($id){
+
+try {
+
+    $id = Crypt::decrypt($id);
+
+} catch (DecryptException $e) {
+
+return redirect()->route('home');
+
+}
+
+return $id;
+
+
+}
 
 
 
