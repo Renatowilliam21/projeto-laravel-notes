285522faaa8e8d0e757a3a3d6655abc37e384b0a
Renato William Rodrigues de Souza <renatowilliam21@gmail.com>
2026-02-12 05:50:50 -0300

    Criando servico de Crypt

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -32,7 +33,9 @@ public function newNote(){
 public function editNote($id){
 
 
-$id = $this->decryptId($id);
+//$id = $this->decryptId($id);
+
+$id = Operations::decryptId($id);
 echo "ESTOU EDITANDO A NOTA CUJO O ID É $id";
 
 }
@@ -40,28 +43,14 @@ echo "ESTOU EDITANDO A NOTA CUJO O ID É $id";
 public function deleteNote($id){
 
 
-$id = $this->decryptId($id);
+//$id = $this->decryptId($id);
+$id = Operations::decryptId($id);
+
 echo "ESTOU DELETANDO A NOTA CUJO O ID É $id";
 
 
 }
 
-private function decryptId($id){
-
-try {
-
-    $id = Crypt::decrypt($id);
-
-} catch (DecryptException $e) {
-
-return redirect()->route('home');
-
-}
-
-return $id;
-
-
-}
 
 
 
